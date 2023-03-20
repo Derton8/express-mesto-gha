ee555f198ca31f0885984d6fd7c89eca683f95ad
Danil Mysenko <danil.mysenko@yandex.ru>
2023-03-20 15:48:04 +0300

    Создать контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,6 +15,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.use(bodyParser.json());
+app.use('/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
