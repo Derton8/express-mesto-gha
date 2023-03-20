3fe7756855b860181e956bd7533ddc47e7fcb908
Danil Mysenko <danil.mysenko@yandex.ru>
2023-03-20 13:03:17 +0300

    Завести express-сервер и настроить его запуск. Подключиться к серверу MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+/* eslint-disable no-console */
 const express = require('express');
+const mongoose = require('mongoose');
 
+const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .then((res) => {
+    console.log(`Соединение с БД установлено. Ответ: ${res.Model}`);
+  });
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
