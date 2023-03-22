f8617f7ab975cdda5c4f071062bfa396cdeb5bb5
Danil Mysenko <danil.mysenko@yandex.ru>
2023-03-22 11:42:49 +0300

    Реализовать обработку ошибок при запросе на сервер

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
 const Card = require('../models/card');
+
+const {
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_OK,
+} = require('../utils/constants');
 
 module.exports.getCards = ((req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(HTTP_STATUS_OK).send({ data: cards }))
+    .catch((err) => res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` }));
 });
 
 module.exports.createCard = ((req, res) => {
@@ -11,34 +19,64 @@ module.exports.createCard = ((req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.status(200).send({ card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(HTTP_STATUS_OK).send({ card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 });
 
 module.exports.deleteCard = ((req, res) => {
   const { cardId } = req.params;
-
+  if (!mongoose.isValidObjectId(cardId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id для удаления карточки.' });
+  }
   Card.findByIdAndDelete(cardId)
+    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с указанным id не найдена.' }))
     .then((card) => res.status(200).send({ card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 });
 
 module.exports.likeCard = ((req, res) => {
   const { cardId } = req.params;
   const userId = req.user._id;
-
+  if (!mongoose.isValidObjectId(cardId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id карточки.' });
+  }
   // $addToSet: добавить _id в массив, если его там нет
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
-    .then((card) => res.status(200).send({ card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с указанным id не найдена.' }))
+    .then((card) => res.status(HTTP_STATUS_OK).send({ card }))
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 });
 
 module.exports.dislikeCard = ((req, res) => {
   const { cardId } = req.params;
   const userId = req.user._id;
-
+  if (!mongoose.isValidObjectId(cardId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id карточки.' });
+  }
   // $pull: убрать _id из массива
   Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
-    .then((card) => res.status(200).send({ card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Карточка с указанным id не найдена.' }))
+    .then((card) => res.status(HTTP_STATUS_OK).send({ card }))
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 });
--- controllers/users.js
+++ controllers/users.js
@@ -1,41 +1,84 @@
+const mongoose = require('mongoose');
 const User = require('../models/user');
+
+const {
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_OK,
+} = require('../utils/constants');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(HTTP_STATUS_OK).send({ data: users }))
+    .catch((err) => res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` }));
 };
 
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
-
+  if (!mongoose.isValidObjectId(userId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id для создания пользователя.' });
+  }
   User.findById(userId)
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь с указанным id не найден.' }))
+    .then((user) => {
+      res.status(HTTP_STATUS_OK).send({ data: user });
+    })
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(HTTP_STATUS_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 };
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
-
-  User.findByIdAndUpdate(userId, { name, about }, { new: true })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+  if (!mongoose.isValidObjectId(userId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id для обновлении пользователя.' });
+  }
+  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
+    .orFail(() => res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Пользователь с указанным id не найден.' }))
+    .then((user) => res.status(HTTP_STATUS_OK).send({ data: user }))
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      if (err.name === 'ValidationError') {
+        res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении пользователя.' });
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
-
+  if (!mongoose.isValidObjectId(userId)) {
+    res.status(HTTP_STATUS_BAD_REQUEST).send({ message: 'Передан некорректный id пользователя для обновлении аватара.' });
+  }
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
-    .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(HTTP_STATUS_OK).send({ data: user }))
+    .catch((err) => {
+      if (res.headersSent) {
+        return;
+      }
+      res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: `Произошла ошибка ${err.name} с текстом: ${err.message}.` });
+    });
 };
