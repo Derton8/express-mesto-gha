f22a26a4df850027b2a1409bcc7c1c9684b72ec3
Danil Mysenko <danil.mysenko@yandex.ru>
2023-04-02 15:20:23 +0300

    fix: Извлечение userId из куки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 
 const { HTTP_STATUS_UNAUTHORIZED } = require('../utils/constants');
 
-module.exports = (res, req, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    res.status(HTTP_STATUS_UNAUTHORIZED).send({ message: 'Необходимо авторизоваться.' });
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    return res.status(HTTP_STATUS_UNAUTHORIZED).send({ message: 'Необходимо авторизоваться.' });
   }
-  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'MY_SECRET_KEY');
